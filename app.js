171a25cb7c48afad510b2637c1fa012a0bbfa019
srinivaas <srinivaasganesan@gmail.com>
2017-10-08 00:25:45 -0600

    second

---
+++ app.js
@@ -0,0 +1,23 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope) {
+  $scope.food = "";
+  $scope.value = "";
+  
+  $scope.setText = function () {  
+    var str = $scope.value
+    var num = str.split(",");
+	
+	if(num.length > 3)
+		$scope.food = "Too Much!";
+	else 
+		$scope.food = "Enjoy!";
+  };
+}
+
+})();
